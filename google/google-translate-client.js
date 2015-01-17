ba7dfd813dd773c8be7acdfd1ed9eaf92c155c5f
Sergey Bolshov <bolszow@gmail.com>
2015-01-17 16:20:33 +0100

    make translation by google

--- google/google-translate-client.js
+++ google/google-translate-client.js
@@ -2,40 +2,73 @@ var request = require('request');
 var _ = require('lodash-node');
 var querystring = require('querystring');
 
-var prepareUrl = function(text) {
-	var opts = {
-		client: 'gtx',
-		sl: 'pl', //source language
-		tl: 'ru', //target language
-		dt: 't', //response data format
-		ie: 'UTF-8',
-		oe: 'UTF-8',
-		q: text
-	};
+var tatoebaFile = 'kde.pl';
+var LineByLineReader = require('line-by-line');
+var lr = new LineByLineReader('/home/sergey/Projects/psi-scripts/reference-translation/kde/' + tatoebaFile);
 
-	return 'http://translate.googleapis.com/translate_a/single?' + querystring.stringify(opts);
+
+var prepareUrl = function(text) {
+    var opts = {
+        client: 'gtx',
+        sl: 'pl', //source language
+        tl: 'ru', //target language
+        dt: 't', //response data format
+        ie: 'UTF-8',
+        oe: 'UTF-8',
+        q: text
+    };
+
+    return 'http://translate.googleapis.com/translate_a/single?' + querystring.stringify(opts);
 };
 
 var getTranslation = function(url, callback) {
-	request(url, function(error, response, body) {
-		if (!error && response.statusCode == 200) {
+    request(url, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
 
-			//remove .."pl" on the end
-			body = body.substring(0, body.length - 7) + "]"; 
-			body = JSON.parse(body);
+            //remove .."pl" on the end
+            body = body.substring(0, body.length - 7) + "]";
+            body = JSON.parse(body);
 
-			var translation = '';
-			_.forEach(body[0], function(arr) {
-				translation += arr[0];
-			});
+            var translation = '';
+            _.forEach(body[0], function(arr) {
+                translation += arr[0];
+            });
 
-			callback(translation);
-		}
-	});
+            callback(translation);
+        }
+    });
 };
 
-var text = 'Google Translate przetłumaczył to na rosyjski!';
+var index = 0;
+var polish = [];
+var russian = [];
 
-getTranslation(prepareUrl(text), function(translation) {
-	console.log(translation);
+lr.on('error', function(err) {
+    console.log(err);
+});
+
+
+lr.on('line', function(line) {
+    polish[index] = line;
+    index++;
+});
+
+lr.on('end', function() {
+
+    var translationComplete = _.after(polish.length, function() {
+        _.forEach(russian, function(line) {
+            console.log(line);
+        });
+
+    });
+
+    _.forEach(polish, function(line, i) {
+        (function(i) {
+            getTranslation(prepareUrl(line), function(translation) {
+                russian[i] = translation;
+                translationComplete();
+                return translation;
+            });
+        })(i);
+    });
 });
