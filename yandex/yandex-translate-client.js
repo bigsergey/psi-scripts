57a1dad1a0f956d542cb85a6c8fe1b1b266efb12
Sergey Bolshov <bolszow@gmail.com>
2015-01-17 16:01:07 +0100

    make translation by yandex

--- yandex/yandex-translate-client.js
+++ yandex/yandex-translate-client.js
@@ -1,20 +1,56 @@
 var translate = require('yandex-translate');
-var util = require('util');
+var _ = require('lodash-node');
+
+var tatoebaFile = 'tatoeba.pl';
+var LineByLineReader = require('line-by-line');
+var lr = new LineByLineReader('/home/sergey/Projects/psi-scripts/reference-translation/tatoeba/' + tatoebaFile);
+
 
 var getTranslation = function(textToTranslate, callback) {
 
-	translate(textToTranslate, {
-		from: 'pl',
-		to: 'ru',
-		key: 'trnsl.1.1.20150114T220515Z.631c19790aa1feca.597002495cf728a0fb30e3df1eaae6fe338d4a08'
-	}, function(err, res) {
-		if (res.code === 200) {
-			callback(res.text);
-		}
-	});
+    translate(textToTranslate, {
+        from: 'pl',
+        to: 'ru',
+        key: 'trnsl.1.1.20150114T220515Z.631c19790aa1feca.597002495cf728a0fb30e3df1eaae6fe338d4a08'
+    }, function(err, res) {
+        if (res.code === 200) {
+            callback(res.text);
+        } else {
+            console.log('Error', err);
+        }
+    });
 };
 
-var translation = getTranslation('Yandex-translate przetłumaczył to na rosyjski!', function (translation) {
-	console.log(translation[0]);
+var index = 0;
+var polish = [];
+var russian = [];
+
+lr.on('error', function(err) {
+    console.log(err);
 });
 
+
+lr.on('line', function(line) {
+    polish[index] = line;
+    index++;
+});
+
+lr.on('end', function() {
+
+    var translationComplete = _.after(polish.length, function() {
+        _.forEach(russian, function(line) {
+            console.log(line);
+        });
+
+    });
+
+    _.forEach(polish, function(line, i) {
+        (function(i) {
+            getTranslation(line, function(translation) {
+                russian[i] = translation[0];
+                translationComplete();
+                return translation[0];
+            });
+        })(i);
+    });
+});
