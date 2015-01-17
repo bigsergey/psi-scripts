b40f3749491dda0362ba247e7079e2a48495a308
Sergey Bolshov <bolszow@gmail.com>
2015-01-17 16:23:16 +0100

    add ref, google and yandex translation

---
+++ reference-translation/ubuntu/Ubuntu.pl-ru.pl
@@ -0,0 +1,229 @@
+Zawiera Gmail, Google Docs, Google+, YouTube oraz Picasa
+Zmiana własnych danych
+Wymagane jest uwierzytelnienie, aby zmienić własne dane
+Zarządzanie kontami użytkowników
+Wymagane jest uwierzytelnienie, aby zmienić dane użytkownika
+Zmiana konfiguracji ekranu logowania
+Wymagane jest uwierzytelnienie, aby zmienić konfigurację ekranu logowania
+Wyświetla informację o wersji i kończy działanie
+Zastępuje istniejące wystąpienie
+Włącza kod debugowania
+Dostarcza interfejsy D-Bus do odpytywania i manipulowania informacjami o kontach użytkowników.
+Składnia:
+\t%s acl ścieżka...
+\t%s -b acl dacl ścieżka...
+\t%s -d dacl ścieżka...
+\t%s -R ścieżka...
+\t%s -D ścieżka...
+\t%s -B ścieżka...
+\t%s -l ścieżka...\t[niekompatybilne z IRIX-em]
+\t%s -r ścieżka...\t[niekompatybilne z IRIX-em]
+%s: błąd przy usuwaniu dostępowego acl dla "%s": %s
+%s: błąd przy usuwaniu domyślnego acl dla "%s": %s
+%s: dostępowe ACL '%s': %s na pozycji %d
+%s: nie można odczytać dostępowego ACL dla '%s': %s
+%s: nie można odczytać domyślnego ACL dla '%s': %s
+%s: nie można odczytać dostępowego tekstu ACL dla '%s': %s
+%s: nie można odczytać domyślnego tekstu ACL dla '%s': %s
+%s: nie można ustawić dostępowego acl dla "%s": %s
+%s: nie można ustawić domyślnego acl dla "%s": %s
+%s: opendir nie powiodło się: %s
+%s: malloc nie powiódł się: %s
+%s: %s: Źle sformułowane dostępowe ACL `%s': %s na pozycji %d
+%s: %s: Źle sformułowane domyślne ACL `%s': %s na pozycji %d
+%s: %s: Tylko katalogi mogą mieć domyślne ACL-e
+%s: %s: Nie znaleziono nazwy pliku w linii %d, przerwanie pracy
+%s: Nie znaleziono nazwy pliku w linii %d standardowego wejścia, przerwanie pracy
+%s: %s: %s w linii %d
+%s: %s: Nie można zmienić właściciela/grupy: %s
+%s %s -- ustawianie list kontroli dostępu do plików (ACL-i)
+Składnia: %s %s
+-m, --modify=acl zmiana aktualnego(ych) ACL(-i) pliku(ów) -M, --modify-file=plik odczyt z pliku pozycji ACL do zmodyfikowania -x, --remove=acl usunięcie wpisów z ACL(-i) pliku(ów) -X, --remove-file=plik odczyt z pliku pozycji ACL do usunięcia -b, --remove-all usunięcie wszystkich rozszerzonych pozycji ACL -k, --remove-default usunięcie domyślnego ACL
+--set=acl ustawienie ACL pliku(ów), zastępując aktualne --set-file=plik odczytanie z pliku pozycji ACL do ustawienia --mask przeliczenie maski efektywnych uprawnień
+-n, --no-mask nie przeliczanie maski efektywnych uprawnień -d, --default wykonywanie operacji na domyślnym ACL
+-R, --recursive rekurencyjne wchodzenie do katalogów -L, --logical przechodzenie logiczne, podążanie za dowiązaniami symbolicznymi -P, --physical przechodzenie fizyczne, nie podążanie za dowiązaniami symbolicznymi --restore=plik odtworzenie ACL-i (odwrotność `getfacl -R') --test tryb testowy (ACL-e nie są modyfikowane)
+%s: Standardowe wejście: %s
+%s: Opcja -%c niekompletna
+%s: Opcja -%c: %s obok znaku %d
+%s: %s w linii %d pliku %s
+%s: %s w linii %d standardowego wejścia
+`%s --help' wyświetli więcej informacji.
+%s: Usunięcie wiodącego '/' ze ścieżek bezwzględnych
+%s %s -- odczyt list kontroli dostępu do plików (ACL-i)
+Składnia: %s [-%s] plik ...
+-d, --default wyświetlenie domyślnej listy kontroli dostępu
+Wiele pozycji tego samego typu
+Powtórzone pozycje
+Brakująca lub błędna pozycja
+Nieprawidłowy typ pozycji
+ustawianie uprawnień dla %s
+zachowywanie uprawnień dla %s
+Menedżer dziennika aktywności
+Konfiguruje zachowanie dziennika aktywności Zeitgeist
+Narzędzie menedżera aktywności i prywatności
+Prywatność i bezpieczeństwo
+Menedżer prywatności i aktywności
+prywatność;dostępność;log;zeitgeist;diagnostyka;błędne raporty;
+Pliki i aplikacje
+Bezpieczeństwo
+Wyszukaj
+Diagnostyka
+Panel wyszukiwania:
+Zawarcie wyników wyszukiwania online
+Wymagaj mojego hasła jeśli:
+_Wybudzanie ze stanu wstrzymania
+_Wróć jeśli będzie pusty ekran
+_Jeśli ekran jest pusty
+Ustawienia hasła
+Ustawienia zasilania
+Nazwa
+Brak opisu
+Ostatnio używane
+Aktywność
+Wybór programu
+Dzisiaj, %H:%M
+Wczoraj, %H:%M
+%e %B %Y, %H:%M
+Nigdy
+Od:
+Do:
+Nieprawidłowy zakres czasu
+%d %B %Y
+Muzyka
+Wideo
+Obrazy
+Dokumenty
+Prezentacje
+Arkusze kalkulacyjne
+Dzienniki rozmów
+Ostatnio używane pliki i programy, mogą być widoczne w Panelu i innych miejscach. Jeśli z jednego konta użytkownika korzysta wiele osób, można ograniczyć rejestrowanie i wyświetlanie tych informacji.
+Nagrywanie plików i użytkowania aplikacji
+Wyczyść dane serwera...
+Wyświetl:
+Wyklucz:
+Usuń element
+Wybierz folder, aby dodać go do czarnej listy
+Wybierz plik, aby dodać go do czarnej listy
+Wyczyść Dane Serwera
+Usuń informacje o używanych plikach i aplikacjach
+W ciągu ostatniej godziny
+W ciągu ostatniego dnia
+W ciągu ostatniego tygodnia
+Wszystko
+To działanie nie może zostać cofnięte. Usunąć tę aktywność?
+Ubuntu może zbierać anonimowe informacje, które pomagają programistom w jego ulepszaniu. Wszystkie zbierane informacje podlegają naszej polityce prywatności.
+Polityka prywatności
+Uprawnienia użytkowników:
+Wysyłanie zgłoszeń błędów do firmy Canonical
+Zgłoszenia błędów zawierają informacje o działaniu programu podczas awarii. Użytkownik może w dowolnej chwili wysłać lub anulować raport.
+Pokaż poprzednie raporty
+Sporadycznie wysyłaj informacje do firmy Canonical
+Przykładowe zgłoszenie zawiera informacje o pojemności dysku komputera, podłączonych urządzeniach czy ilości aktualnie uruchomionych programów.
+Tylko administrator może dodawać użytkownika lub grupę do systemu.
+Dozwolone podanie tylko jednej lub dwóch nazw.
+W tym trybie można podać tylko jedną nazwę.
+Opcje --group, --ingroup oraz --gid są wzajemnie wykluczające się.
+Katalog domowy musi być podany jako ścieżka bezwzględna.
+Ostrzeżenie: Podany katalog domowy %s już istnieje.
+Ostrzeżenie: Brak dostępu do podanego katalogu domowego %s: %s
+Grupa "%s" już istnieje jako grupa systemowa. Kończenie.
+Grupa "%s" już istnieje i nie jest grupą systemową. Kończenie.
+Grupa "%s" już istnieje, ale ma inny GID. Kończenie.
+GID "%s" jest już używany.
+Brak dostępnego numeru GID w zakresie %d-%d (FIRST_SYS_GID - LAST_SYS_GID).
+Nie utworzono grupy "%s".
+Dodawanie grupy "%s" (GID %d)...
+Gotowe.
+Grupa "%s" już istnieje.
+Brak dostępnego numeru GID w zakresie %d-%d (FIRST_GID - LAST_GID).
+Użytkownik "%s" nie istnieje.
+Grupa "%s" nie istnieje.
+Użytkownik "%s" jest już członkiem grupy "%s".
+Dodawanie użytkownika "%s" do grupy "%s"...
+Użytkownik systemowy "%s" już istnieje. Kończenie.
+Użytkownik "%s" już istnieje. Kończenie.
+Użytkownik "%s" już istnieje, ale ma inny UID. Kończenie.
+Brak dostępnego UID/GID w zakresie %d-%d (FIRST_SYS_UID - LAST_SYS_UID).
+Nie utworzono użytkownika "%s".
+Brak dostępnego UID w zakresie %d-%d (FIRST_SYS_UID - LAST_SYS_UID).
+Błąd wewnętrzny
+Dodawanie użytkownika systemowego "%s" (UID %d)...
+Dodawanie nowej grupy "%s" (GID %d)...
+Dodawanie nowego użytkownika "%s" (UID %d) w grupie "%s"...
+"%s" zwrócił kod błędu %d. Kończenie.
+`%s' zakończony z powodu sygnału %d. Kończenie.
+Niepowodzenie %s - kod wyjścia 15, maskowane hasła nie są włączone, nie można ustawić wieku hasła. Kontynuowanie.
+Dodawanie użytkownika "%s"...
+Brak dostępnego UID/GID w zakresie %d-%d (FIRST_UID - LAST_UID).
+Brak dostępnego UID w zakresie %d-%d (FIRST_UID - LAST_UID).
+Dodawanie nowej grupy "%s" (%d)...
+Dodawanie nowego użytkownika "%s" (%d) w grupie "%s"...
+Dostęp zabroniony
+niepoprawna kombinacja opcji
+niespodziewany błąd, nic nie zrobiono
+niespodziewany błąd, brak pliku passwd
+plik passwd zajęty, proszę spróbować ponownie
+niepoprawny argument opcji locale noexpr
+Spróbować ponownie? [t/N] locale yesexpr
+Czy informacja jest poprawna? [T/n]
+Dodawanie nowego użytkownika "%s" do grup dodatkowych...
+Ustawianie kwot dyskowych dla użytkownika "%s" na wartości takie, jak ma użytkownik "%s"...
+Nie utworzono katalogu domowego "%s".
+Katalog domowy "%s" już istnieje. Nie zostanie skopiowany z "%s".
+Ostrzeżenie: katalog domowy "%s" nie należy do właśnie tworzonego użytkownika
+Tworzenie katalogu domowego "%s"...
+Nie utworzono katalogu domowego "%s": %s
+Ustawianie szyfrowania ...
+Kopiowanie plików z "%s" ...
+niepowodzenie fork dla polecenia "find": %s
+Użytkownik "%s" już istnieje i nie jest użytkownikiem systemowym.
+Użytkownik "%s" już istnieje.
+UID %d jest już używany.
+GID %d jest już używany.
+GID %d nie istnieje.
+Nie można przetworzyć %s. Nie jest to ani katalog, ani plik, ani dowiązanie symboliczne.
+%s: Aby uniknąć problemów, nazwa użytkownika powinna składać się tylko z liter, cyfr, podkreśleń, kropek, znaków @ i myślników oraz nie powinna zaczynać się od myślnika (patrz IEEE Std 1003.1-2001). W celu zachowania zgodności z kontami Samby, znak $ jest także wspierany na końcu nazwy użytkownika
+Pozwolenie na użycie kiepskiej jakości nazwy użytkownika.
+Wybieranie UID z zakresu od %d do %d...
+Wybieranie GID z zakresu od %d do %d...
+Zatrzymano: %s
+Usuwanie katalogu "%s" ...
+Usuwanie użytkownika "%s" ...
+Usuwanie grupy "%s" ...
+Otrzymano sygnał SIG%s.
+adduser w wersji %s
+Dodaje użytkownika lub grupę do systemu. Copyright (C) 1997, 1998, 1999 Guy Maor <maor\@debian\.org> Copyright (C) 1995 Ian Murdock <imurdock\@gnu\.ai\.mit\.edu>, Ted Hajek <tedhajek\@boombox\.micro\.umn\.edu>
+Ten program jest darmowy; możesz go redystrybuować i/lub modyfikować zgodnie z warunkami Ogólnej Licencji Publicznej GNU opublikowanej przez Fundację Wolnego Oprogramowania; na zasadach albo wersji 2 tej licencji, albo (wg Twojego uznania) jakiejkolwiek późniejszej wersji. Ten program jest dystrybuowany w nadziei, że będzie użyteczny, ale autorzy NIE DAJĄ ŻADNYCH GWARANCJI; w tym gwarancji PRZYDATNOŚCI DO SPRZEDAŻY ani DO KONKRETNYCH CELÓW. Szczegóły można znaleźć w Ogólnej Licencji Publicznej GNU w pliku /usr/share/common-licenses/GPL.
+--help" and "--version
+Tylko administrator może usunąć użytkownika lub grupę z systemu.
+Po nazwie nie można dodawać żadnych opcji.
+Wprowadź nazwę grupy do usunięcia:
+Wprowadź nazwę użytkownika do usunięcia:
+Aby użyć opcji --remove-home, --remove-all-files oraz --backup, należy zainstalować pakiet "perl-modules". W tym celu uruchom apt-get install perl-modules
+Użytkownik "%s" nie użytkownikiem systemowym. Kończenie.
+Użytkownik "%s" nie istnieje, a podano opcję --system. Kończenie.
+OSTRZEŻENIE: Zamierzasz usunąć konto administratora (root, uid 0).
+Zazwyczaj nigdy nie jest to potrzebne i może popsuć cały system.
+Aby naprawdę to zrobić, prosimy uruchomić deluser z opcją --force.
+Kończenie bez wykonywania żadnej akcji.
+Szukanie plików do kopii bezpieczeństwa/usunięcia ...
+niepowodzenie fork dla polecenia "mount" parsującego punkty montowania: %s
+nie można zamknąć potoku polecenia "mount": %s
+"%s" nie będzie archiwizowany/usuwany, gdyż jest punktem montowania.
+"%s" nie będzie archiwizowany/usuwany, gdyż pasuje do %s.
+Nie można obsłużyć pliku specjalnego %s
+Tworzenie kopii bezpieczeństwa usuwanych plików %s ...
+Usuwanie plików ...
+Usuwanie pliku crontab ...
+Ostrzeżenie: grupa "%s" nie ma już żadnych członków.
+niepowodzenie getgrnam "%s". Nie powinno się to było zdarzyć.
+Grupa "%s" nie jest grupą systemową. Kończenie.
+Grupa "%s" nie jest pusta!
+"%s" wciąż ma "%s" jako grupę podstawową!
+Nie można usunąć użytkownika z jego/jej podstawowej grupy.
+Użytkownik "%s" nie jest członkiem grupy "%s".
+Usuwanie użytkownika "%s" z grupy "%s" ...
+deluser w wersji %s
+Usuwa użytkowników i grup z systemu.
+Copyright (C) 2000 Roland Bauerschmidt <roland\@copyleft\.de>
+deluser jest oparty na adduser Guya Maora <maor\@debian\.org>, Iana Murdocka <imurdock\@gnu\.ai\.mit\.edu> i Teda Hajeka <tedhajek\@boombox\.micro\.umn\.edu>
